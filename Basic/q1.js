a632f66ddcdd38a3ab4d37ede26e54a42fa2755a
VINAY7484 <70506939+VINAY7484@users.noreply.github.com>
2024-08-08 21:08:01 +0530

    first commit

---
+++ Basic/q1.js
@@ -0,0 +1,17 @@
+// Question:1 The sum of all natural numbers from 1 to n
+// Solution:1
+//  function sumOfNaturalNumber(num){
+//     let sum = 0;
+//     for (let i=1; i<=num; i++){
+//         sum += i;
+//     }
+//      console.log(sum)
+//  }
+// Solution:2
+ function sumOfNaturalNumber(num){
+    
+        let sum = num*(num+1)/2
+     console.log(sum)
+ }
+
+ sumOfNaturalNumber(3)
---
+++ Basic/q2.js
@@ -0,0 +1,2 @@
+// Question:2 The sum of all natural numbers from 1 to n
+// Solution:1
